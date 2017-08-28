e621fe393ed47fdfa494babb8ae51e81fd5ceba4
Hydra008 <kshah2@lakeheadu.ca>
2017-08-28 13:04:32 -0400

    Update Application.java

--- Connect to DB Using Java/Challenge 1/Application.java
+++ Connect to DB Using Java/Challenge 1/Application.java
@@ -1,17 +1,9 @@
-public class Application
-{
-  public static void main(String args[])
-  {
-    // Load class "org.sqlite.JDBC"
-    Class.forName("org.sqlite.JDBC");
-    // Establish connection to database named "treehouse.db"
-    try(Connection c = DriverManager.getConnection("jdbc:sqlite:treehouse.db")) 
-    {
-
-    } 
-    catch(SQLException ex) 
-    {
-
-    }
-  }
+// Load class "org.sqlite.JDBC"
+Class.forName("org.sqlite.JDBC");
+// Establish connection to database named "treehouse.db"
+try(Connection c = DriverManager.getConnection("jdbc:sqlite:treehouse.db")) {
+  
+      
+} catch(SQLException ex) {
+      
 }
